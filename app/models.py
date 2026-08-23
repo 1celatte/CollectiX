d7390ed2e24a63bc3811326e2897f2da9f1e8f74
coco-git-hub <cngchifei@gmail.com>
2026-08-23 23:23:50 +0800

    implement user registration

--- app/models.py
+++ app/models.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 from app.extensions import db
+from flask_login import UserMixin
 
-
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
