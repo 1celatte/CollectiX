df8b6060a3edf3afa169614198ca34ef4d607bf2
Chan Zi Jian <zj3yaaa@email.com>
2026-08-26 20:44:19 +0800

    Add collection search filters and sorting

--- app/browse/routes.py
+++ app/browse/routes.py
@@ -1,7 +1,58 @@
-from flask import render_template
+from flask import render_template, request
 from app.browse import browse_bp
+from app.models import Collection
 
+#Browse all public collections
 @browse_bp.route("/")
-def index():
-    #Temporary response for testing
-    return render_template("browse/browse.html")
+def browse_page():
+    #Get the keyword from the search box
+    query = request.args.get("q","").strip()
+    
+    #Get the selected category
+    category = request.args.get("category", "").strip()
+
+    #Get the selected sorting option
+    sort = request.args.get("sort", "newest")
+    
+    #Start with approved public collections
+    collections_query = Collection.query.filter_by(
+        status="approved"
+        )
+    
+    #Search by collection name
+    if query:
+        collections_query = collections_query.filter(
+            Collection.name.ilike(f"%{query}%")
+        )
+         
+    # Filter by category
+    if category:
+        collections_query = collections_query.filter(
+            Collection.category.ilike(category)
+        )
+
+    #Sort the results
+    if sort == "oldest":
+        collections_query = collections_query.order_by(
+            Collection.created_at.asc()
+        )
+    elif sort == "name":
+        collections_query = collections_query.order_by(
+            Collection.name.asc()
+        )
+    else:
+        collections_query = collections_query.order_by(
+            Collection.created_at.desc()
+        )
+
+    #Run the database query
+    collections = collections_query.all()
+
+    #Send all the collections(results) to HTML page
+    return render_template(
+        "browse/browse.html",
+        collections=collections,
+        query=query,
+        category=category,
+        sort=sort
+    )
